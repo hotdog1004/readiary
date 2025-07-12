da54643b96e2240357764b44e73bf45bdecd883b
Gyeol Park <gyeol.dev@gmail.com>
2025-07-12 22:52:28 +0900

    feat: Textfield, NumberField 컴포넌트 추가

---
+++ src/shared/ui/textField/types.ts
@@ -0,0 +1,6 @@
+export interface BaseFieldProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
+  onClear?: () => void
+  placeholder?: string
+  showClearButton?: boolean
+}
