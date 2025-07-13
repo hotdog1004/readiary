a0a27a5a562e6f5fc14b09fcdc4deac474067860
Gyeol Park <gyeol.dev@gmail.com>
2025-07-13 15:20:57 +0900

    refactor: 조건문 로직 명시적인 표현으로 변경

--- src/features/add-book/schemas/reviewSchema.ts
+++ src/features/add-book/schemas/reviewSchema.ts
@@ -7,7 +7,7 @@ export const ReviewSchema = (rating: number) =>
     })
     .superRefine((data, ctx) => {
       if (rating === 1 || rating === 5) {
-        if (!data.review || data.review.length < 100) {
+        if (data.review === undefined || data.review.length < 100) {
           ctx.addIssue({
             path: ['review'],
             message: '독후감은 최소 100자 이상 작성해야합니다.',
